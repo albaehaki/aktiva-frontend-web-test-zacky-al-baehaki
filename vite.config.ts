e06bacfd8996001d2d42f4f50dcd446c02b60250
albaehaki <zackyalbaehaki@gmail.com>
2023-06-28 10:59:26 +0700

    perbaikan pada .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,60 @@
 import { defineConfig } from "vitest/config"
+import { loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
-  },
-  build: {
-    outDir: "build",
-    sourcemap: true,
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
-    mockReset: true,
-  },
-  define: {
-    'process.env': {
-      VITE_YELP_API_TOKEN: JSON.stringify('your token'),
-      VITE_YELP_API_URL: JSON.stringify('url api'),
+// // https://vitejs.dev/config/
+// export default defineConfig(({ command, mode }) => {
+//   const env = loadEnv(mode, process.cwd(), '')
+//   return {
+//   plugins: [react()],
+//   server: {
+//     open: true,
+//   },
+//   build: {
+//     outDir: "build",
+//     sourcemap: true,
+//   },
+//   test: {
+//     globals: true,
+//     environment: "jsdom",
+//     setupFiles: "src/setupTests",
+//     mockReset: true,
+//   },
+//   define: {
+//     'process.env': {
+//       VITE_YELP_API_TOKEN: JSON.stringify('your token'),
+//       VITE_YELP_API_URL: JSON.stringify('url api'),
+//     },
+//   },
+//   },
+// })
+// dotenv.config();
+
+export default (({ command, mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      open: true,
     },
-  },
-})
+    build: {
+      outDir: "build",
+      sourcemap: true,
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "src/setupTests",
+      mockReset: true,
+    },
+    define: {
+      'process.env': {
+        VITE_YELP_API_TOKEN: JSON.stringify('your token'),
+        VITE_YELP_API_URL: JSON.stringify('url api'),
+      },
+    }
+  })
+});
